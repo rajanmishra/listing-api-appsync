197ba42e91f37681426de371d791a73caca1d87e
Rajan Mishra <rajanmishra2010@hotmail.com>
2024-06-13 19:28:56 +0530

    Update aws.js
    Added additional information

--- aws.js
+++ aws.js
@@ -1,4 +1,5 @@
 const AWS = require('aws-sdk');
+// This configuration can be managed by lambda variable or by giving right permission to role used to create lambda
 AWS.config.update({
     accessKeyId: process.env.AWS_KEY,
     secretAccessKey: process.env.AWS_SECRET,
